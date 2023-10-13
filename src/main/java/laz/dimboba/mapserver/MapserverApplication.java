8adc3c01e10c8afe4464bd0522d2aade9500affa
Dimboba <lazarkovdmitry@yandex.ru>
2023-10-13 21:39:56 +0300

    Initial commit

---
+++ src/main/java/laz/dimboba/mapserver/MapserverApplication.java
@@ -0,0 +1,13 @@
+package laz.dimboba.mapserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapserverApplication.class, args);
+    }
+
+}
