593667cd11448a0f24f190f5c28e49cffa4e686d
Dimboba <lazarkovdmitry@yandex.ru>
2023-10-13 23:29:15 +0300

    feat: atm entity

---
+++ src/main/java/laz/dimboba/mapserver/atm/Service.java
@@ -0,0 +1,12 @@
+package laz.dimboba.mapserver.atm;
+
+public enum Service {
+    supportsEur,
+    supportsRub,
+    supportsUsd,
+    supportsChargeRub,
+    blind,
+    nfcForBankCards,
+    qrRead,
+    wheelchair
+}
