923090b67e0b583398c321e61ba4a0e32e29d1fa
Dimboba <lazarkovdmitry@yandex.ru>
2023-10-13 23:32:15 +0300

    fix: user entity

--- src/main/resources/db/migration/V1__createUsers.sql
+++ src/main/resources/db/migration/V1__createUsers.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
     number VARCHAR(64) UNIQUE NOT NULL,
     password VARCHAR(64) NOT NULL,
     name VARCHAR(64),
-    address VARCHAR(255),
+--    address VARCHAR(255),
 --     address_latitude DOUBLE PRECISION,
 --     address_longitude DOUBLE PRECISION,
 
